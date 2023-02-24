9afdd8dd29198934c036d17349bc5c8ab515ee94
Wandwilson Almeida da silva <77290358+wandyalmeida@users.noreply.github.com>
2023-02-24 15:37:09 +0000

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,29 @@
+//Adding function to calculator
+function Sum(){
+    var num = parseFloat(document.querySelector("#FirstNumber").value);
+    var num2 = parseFloat(document.querySelector("#SecondNumber").value);
+    document.getElementById("Result").innerHTML= num + num2;
+
+
+}
+function Sub(){
+    var num = parseFloat(document.querySelector("#FirstNumber").value);
+    var num2 = parseFloat(document.querySelector("#SecondNumber").value);
+    document.getElementById("Result").innerHTML= num - num2;
+
+
+}
+function Mult(){
+    var num = parseFloat(document.querySelector("#FirstNumber").value);
+    var num2 = parseFloat(document.querySelector("#SecondNumber").value);
+    document.getElementById("Result").innerHTML= num * num2;
+
+
+}
+function Divide(){
+    var num = parseFloat(document.querySelector("#FirstNumber").value);
+    var num2 = parseFloat(document.querySelector("#SecondNumber").value);
+    document.getElementById("Result").innerHTML= num / num2;
+
+
+}
